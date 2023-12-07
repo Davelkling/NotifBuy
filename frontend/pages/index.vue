3a50ca7ba67b918fdc91ef21155b2c8fc95b464a
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-12-07 16:58:37 +0800

    Add signin route and Signin component

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,9 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div>
-        <Header/>
-    </div>
+  <div>
+    <Header />
+  </div>
 </template>
 
-
-
-<style scoped>
-
-</style>
+<style scoped></style>
